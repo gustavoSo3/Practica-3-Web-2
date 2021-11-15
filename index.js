f68f08a6a45337240c0cb59606f6571b07e62c71
Gustavo Sotres <gustavonline.games@gmail.com>
2021-11-14 21:03:51 -0600

    Imported dependencies

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+const mongoose = require('mongoose');
