717685ac2dadcb8dc6a9296d6f32640aa91a2330
Gustavo Sotres <gustavonline.games@gmail.com>
2021-11-14 23:25:36 -0600

    Updated Chanels Schema

--- src/models/Chanels.js
+++ src/models/Chanels.js
@@ -4,6 +4,13 @@ const channels_schema = new mongoose.Schema({
 	id_user: {
 		type: String,
 		require: true
+	},
+	name: {
+		type: String,
+		require: true
+	},
+	invite_link: {
+		type: String
 	}
 }, { timestamps: true });
 module.exports = mongoose.model('channels', channels_schema);
